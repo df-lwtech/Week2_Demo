06d08341b5c4a22d5184b1b91cdecda8817171ff
David Ferguson <44074746+df-lwtech@users.noreply.github.com>
2025-10-09 18:41:49 -0700

    Destroying app.js
    Cause, why not?

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello, world!');
-});
+  
 
 server.listen(3000, () => {
   console.log('Server running at http://localhost:3000');
